a4f5c0f299b5a59aa804c77c241fe1794b2aebab
HangmansMoose <trivial86@protonmail.com>
2025-09-13 13:51:38 +1000

    back on main

--- src/main.zig
+++ src/main.zig
@@ -6,8 +6,11 @@ pub fn main() !u8 {
     const wad_filename = "DOOM.WAD";
 
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    var wadloader: WADLoader = undefined;
-    try wadloader.Load(&gpa, wad_filename);
+    var arena = std.heap.ArenaAllocator.init(gpa.allocator());
+    defer arena.deinit();
+    const allocator = arena.allocator();
+    var wadloader: WADLoader = try .init(allocator, wad_filename);
+    try wadloader.OpenAndLoad();
 
     //const map = Map.init("E1M1");
     //try wadloader.LoadMapData(map);
--- src/wadloader.zig
+++ src/wadloader.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 const Datatypes = @import("datatypes.zig");
-//const WADReader = @import("wadreader.zig").WADReader;
+const WADReader = @import("wadreader.zig").WADReader;
 const Map = @import("map.zig");
 const WADDirectory = Datatypes.WADDirectory;
 const WADHeader = Datatypes.WADHeader;
@@ -12,78 +12,52 @@ const Linedef = Datatypes.Linedef;
 
 pub const WADLoader = struct {
     // wad_path: std.fs.Dir, // Directory where WADs are stored
-    wad_data: std.array_list.Managed([]u8) = undefined,
-    wad_file: std.fs.File = undefined,
+    wad_buffer: []u8,
     wad_allocator: std.mem.Allocator,
-    wad_header: *WADHeader = undefined,
-    wad_directories: std.array_list.Managed(WADDirectory) = undefined,
+    wad_file: std.fs.File,
+    wad_header: WADHeader,
+    wad_reader: WADReader,
+    wad_directories: std.array_list.Managed(WADDirectory),
 
-    pub fn Load(self: *WADLoader, gpa: *std.heap.GeneralPurposeAllocator(.{}), wad_filename: []const u8) !void {
-        // TODO: this will be a thing once I get everything else working
-        _ = wad_filename;
-        self.wad_allocator = gpa.allocator();
+    const Self = @This();
 
-        // const cwd = std.fs.cwd();
-        // const path_str = try cwd.realpathAlloc(self.wad_allocator, ".");
-        // std.debug.print("cwd: {s}\n", .{path_str});
+    pub fn init(allocator: std.mem.Allocator, file_to_load: []const u8) !Self {
+        _ = file_to_load;
 
-        const wad_rel_path = "assets/DOOM.WAD";
-        self.wad_file = try std.fs.cwd().openFile(wad_rel_path, .{});
-        defer self.wad_file.close();
+        var loader = Self{
+            .wad_buffer = undefined,
+            .wad_allocator = allocator,
+            .wad_file = try std.fs.cwd().openFile("assets/DOOM.WAD", .{}),
+            .wad_header = undefined,
+            .wad_reader = undefined,
+            .wad_directories = undefined,
+        };
 
-        //self.wad_data = try self.wad_allocator.alloc(u8, try self.wad_file.getEndPos());
-        //self.wad_directories = std.array_list.Managed(WADDirectory).init(self.wad_allocator);
+        const file_stat = try loader.wad_file.stat();
+        loader.wad_buffer = try allocator.alloc(u8, file_stat.size);
 
-        // TODO: Look at errors and their implementation
-        try self.ReadInDirectories();
-        // try self.OpenAndLoadWAD();
+        // Instatiate the directories list
 
-        // try self.ReadDirectories();
+        return loader;
     }
 
-    fn ReadInDirectories(self: *WADLoader) !void {
-        // Read in header
-        // NOTE: I dont need to store the header once the dirs are loaded I dont think I need to
-        // care about the header anymore
-        // const buffer = try self.wad_allocator.alloc(u8, try self.wad_file.getEndPos());
+    pub fn deinit(self: *Self) void {
+        self.wad_allocator.free(self.wad_buffer);
+    }
 
-        var reader: std.Io.Reader = undefined;
+    pub fn OpenAndLoad(self: *Self) !void {
         const file_stat = try self.wad_file.stat();
-        const file_size = file_stat.size;
+        self.wad_buffer = try self.wad_file.readToEndAlloc(self.wad_allocator, file_stat.size);
+        self.wad_header = self.wad_reader.ReadHeaderData(&self.wad_buffer, 0);
+        self.wad_directories = std.array_list.Managed(WADDirectory).init(self.wad_allocator);
+        try self.ReadDirectories();
+    }
 
-        reader = std.Io.Reader.fixed(try self.wad_file.readToEndAlloc(self.wad_allocator, file_size));
-
-        var header = try self.wad_allocator.alloc(WADHeader, 1);
-        header = try reader.takeStruct(WADHeader, .little);
-        self.wad_header = &header;
-        //var header_buffer: [@sizeOf(WADHeader)]u8 align(@alignOf(WADHeader)) = undefined;
-        //_ = try self.wad_file.read(&header_buffer);
-
-        //self.wad_header = @ptrCast(&header_buffer);
-
-        const wad_ident_str: []const u8 = @ptrCast(&self.wad_header.wad_type);
-        std.debug.print("WAD type: {s}\n", .{wad_ident_str});
-        std.debug.print("Dir count: {d}\n", .{self.wad_header.directory_count});
-        std.debug.print("First dir offset: {d}\n", .{self.wad_header.directory_offset});
-
+    fn ReadDirectories(self: *Self) !void {
         for (0..self.wad_header.directory_count) |i| {
             // TODO: not sure where the * 16 comes from. Need to go back through it
-            try self.ReadInDirectory(self.wad_header.directory_offset + i * 16);
+            const dir = self.wad_reader.ReadDirectoryData(&self.wad_buffer, self.wad_header.directory_offset + i * 16);
+            try self.wad_directories.append(dir);
         }
-    }
-
-    fn ReadInDirectory(self: *WADLoader, offset: usize) !void {
-        // const offset: u64 = self.wad_header.directory_offset;
-        var buffer: [@sizeOf(WADDirectory)]u8 align(@alignOf(WADDirectory)) = undefined;
-        _ = try self.wad_file.seekTo(offset);
-        _ = try self.wad_file.read(&buffer);
-
-        const directory: *WADDirectory = @ptrCast(&buffer);
-        try self.wad_directories.append(directory.*);
-
-        const dir_name_str: [:0]const u8 = @ptrCast(&directory.lump_name);
-        std.debug.print("Lump name: {s}\n", .{dir_name_str});
-        std.debug.print("Lump size: {d}\n", .{directory.lump_size});
-        std.debug.print("Lump offset: {d}\n", .{directory.lump_offset});
     }
 };
